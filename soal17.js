c8012ac109a7846051fcdc1328504d643e9bc832
accenggg <arseliusngiklan@gmail.com>
2023-09-05 14:30:08 +0800

    menjelaskan tiap line program

--- soal17.js
+++ soal17.js
@@ -12,16 +12,29 @@
 
 function solution(n) {
     // tulis jawabanmu disini
+    // 1. mengecek input nilai n apabila diantara 91-100 maka jalankan program
     if (n > 90 && n <= 100){
         return "A"
-    } else if (n > 80 && n <= 90){
+    }
+    // 2. mengecek input nilai n apabila diantara 81-90 maka jalankan program 
+    else if (n > 80 && n <= 90){
         return "B"
-    } else if (n > 70 && n <= 80){
+    } 
+    // 3. mengecek input nilai n apabila diantara 71-80 maka jalankan program
+    else if (n > 70 && n <= 80){
         return "C"
-    } else if (n >60 && n <= 70){
+    } 
+    // 4. mengecek input nilai n apabila diantara 61-70 maka jalankan program
+    else if (n >60 && n <= 70){
         return "D"
-    } else {
+    } 
+    // 5. mengecek input nilai n apabila kurang dari sama dengan 60 jalankan program
+    else if(n <= 60) {
         return "E"
+    } 
+    // 6. jalankan program jika tidak ada syarat if dan else if yang terpenuhi
+    else {
+        return "Masukan nilai dengan range 1 - 100"
     }
 }
 

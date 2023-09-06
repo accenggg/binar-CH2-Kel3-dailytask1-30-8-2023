4748e5ff4bd7784c85d8e75de3231c3ea749c3fe
accenggg <arseliusngiklan@gmail.com>
2023-09-06 13:36:03 +0800

    penyelesaian soal nomor 8 dan menambahkan penjelasan per line

--- soal8.js
+++ soal8.js
@@ -1,23 +1,22 @@
+// tentukan user yang company huruf depan nya berawal L dan ada berapa jumlah user nya
+
 const users = require("./users")
 
 const checkFirstCharacterOfCompany = (data) => {
+    // 1. membuat variabel result untuk menampung hasil fungsi
     const result = []
-
+    // 2. melakukan perulangan untuk mengecek data user satu persatu
     for(let i=0; i < data.length; i++){
-        if(data[i].company.charAt(0) === "L"){
+        // 3. mengecek data company setiap user yang berawal huruf L, maka jalankan program
+        if(data[i].company[0] === "L"){
+            // 4. menambahkan data user kedalam variabel result
             result.push(data[i])
         }
     }
 
+    // 5. mengembalikan hasil
     const totalData = result.length
     return {result, totalData}
 }
 
-const data = checkFirstCharacterOfCompany(users)
-
-const resultData = data.result
-
-const totalData = data.totalData
-
-console.log({resultData});
-console.log({totalData});
+console.log(checkFirstCharacterOfCompany(users));

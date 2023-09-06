bc1bd5acaac2cef4c64f334ee0b7c95fbae89666
accenggg <arseliusngiklan@gmail.com>
2023-09-06 19:32:22 +0800

    menambahkan pengecekan saldo saat ini

--- soal16.js
+++ soal16.js
@@ -8,15 +8,22 @@ function solution(x,y) {
     // tulis jawabanmu disini
     // 1. mengecek input user, apabila input x habis dibagi 5, maka jalankan program didalam block
     if (x % 5 === 0){
-        // 2. simpan biaya admin kedalam variabel biayaAdmin
-        const biayaAdmin = 0.5
-        // 3. melakukan perhitungan mengurangkan jumlah uang - uang yang ditarik - biaya admin
-        let sisaSaldo = y - x - biayaAdmin
-        // 4. mengembalikan sisaSaldo yang sudah diubah
-        return sisaSaldo
-    // 5. jika x tidak habis dibagi 5
+        // 2. mengecek apakah saldo lebih besar dari uang yang ingin ditarik
+        if (x > y){
+            return "Saldo tidak cukup untuk melakukan penarikan"
+        }
+        // 3. jika syarat tidak terpenuh, jalankan program
+        else{
+            // 4. simpan biaya admin kedalam variabel biayaAdmin
+            const biayaAdmin = 0.5
+            // 5. melakukan perhitungan mengurangkan jumlah uang - uang yang ditarik - biaya admin
+            let sisaSaldo = y - x - biayaAdmin
+            // 6. mengembalikan sisaSaldo yang sudah diubah
+            return sisaSaldo
+        }
+    // 7. jika x tidak habis dibagi 5
     } else {
-        // 6. mengembalikan sisaSaldo yang sudah diubah
+        // 8. mengembalikan sisaSaldo yang sudah diubah
         return y
     }
 }

3e66d9d9d9fb4371e64d775fcdb44f2993471424
accenggg <arseliusngiklan@gmail.com>
2023-09-06 11:44:43 +0800

    menambahkan penjelasan per line

--- soal2.js
+++ soal2.js
@@ -2,11 +2,16 @@
 const users = require("./users")
 
 const genderCheck = (data) => {
+    // 1. menyiapkan variabel untuk menampung hasil
     let result = []
     let totalFemale = 0;
+    // 2. melakukan perulangan untuk mengecek data user satu persatu
     for (let i = 0; i < data.length; i++){
+        // 3. mengecek jika gender user adalah female, jalankan program
         if(data[i].gender === "female"){
+            // 4. menambahkan 1 ke value totalFemale
             totalFemale += 1
+            // 5. menambahkan data user kedalam variabel result
             result.push({
                 id : data[i]._id,
                 name : data[i].name,  
@@ -14,10 +19,12 @@ const genderCheck = (data) => {
         })
         }
     }
+    // 6. mengembalikan hasil
     return {
         result,
         total : totalFemale
     }
 }
 
+// 7. menampilkan hasil
 console.log(genderCheck(users));
